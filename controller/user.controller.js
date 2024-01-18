ae0914ddc729b67cbc0ea7619e1ae484dc40a010
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-18 15:30:12 +0530

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -52,11 +52,7 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-   res.header('Access-Control-Allow-Origin',process.env.CORS_ORIGIN);
-  res.header('Access-Control-Allow-Methods', 'POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true');
-
+ 
   const { email, password } = req.body;
   //checking empty fields
   if (!email || !password) {
