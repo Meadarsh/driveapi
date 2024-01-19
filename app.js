57a56b6fe9485e15ca53ffdff1d335d916df8481
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-20 02:18:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ import cookieParser from "cookie-parser"
 import fileUpload from "express-fileupload";
 const app=express()
 import fs from "fs";
+import os from "os";
+import path from "path";
 const directory = 'temp';
 
-if (!fs.existsSync(directory)) {
-    fs.mkdirSync(directory);
+const tempDirPath = path.join(os.tmpdir(), directory);
+
+if (!fs.existsSync(tempDirPath)) {
+  try {
+    fs.mkdirSync(tempDirPath);
+    console.log('Temporary directory created:', tempDirPath);
+  } catch (error) {
+    console.error('Error creating temporary directory:', error);
+  }
+} else {
+  console.log('Temporary directory already exists:', tempDirPath);
 }
 
     app.use(fileUpload(
