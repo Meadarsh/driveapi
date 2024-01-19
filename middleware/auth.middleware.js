3b0ca6eff30a5e22f0dae95f72edf34865d00d3b
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-19 18:48:54 +0530

    Update auth.middleware.js

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -3,8 +3,7 @@ import { User } from "../models/users.model.js";
 const auth = (req, res, next) => {
   try {
     let token = req.cookies.accessToken;
-    if (!token)
-      return res.status(401).send({ message: "Unauthorized!", auth: false });
+    if (!token) {return res.status(401).send({ message: "Unauthorized!", auth: false });}
     const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
   // console.log("verified", verified);
     const user = User.findById(verified._id);
