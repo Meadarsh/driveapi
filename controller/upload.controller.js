55bb57682b4871ed90b627715cab8a93a7e651ca
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-21 02:22:46 +0530

    Update upload.controller.js

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -4,7 +4,7 @@ import { uploadOnCloudinary } from "../utility/cloudinary.js";
 import { File } from "../models/file.model.js";
 const uploadFile = asyncHandler(async(req, res) => {
   try {
-    if(!req.files) return res.status(400).json({message:"Select a file"})
+    if(!req.files) {return res.status(400).json({message:"Select a file"})}
     const path=req.files?.files.tempFilePath
   /**********   Storage calculate      ****************************************************** */
  const user = await User.findOne({email:req.user.email})
