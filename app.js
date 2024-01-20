0a6fa36ce33b519d77fa03f20443ed1708a9bec6
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-21 02:19:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,26 +10,23 @@ const directory = 'tmp';
 
 
 const currentWorkingDirectory = process.cwd();
-const tempDir = path.join(currentWorkingDirectory, 'temp'); // Adjust the folder name as needed
+const tempDir = path.join(currentWorkingDirectory, 'myAppTemp');
 
 try {
   // Create the directory and its parent directories if they don't exist
   fs.mkdirSync(tempDir, { recursive: true });
   console.log(`Directory '${tempDir}' created successfully.`);
 
-  // Your application logic goes here...
-
-} catch (error) {
+  // Configure express-fileupload to use the correct temporary directory
+  app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: tempDir,
+    })
+  );} catch (error) {
   // Handle any errors that occur during directory creation
   console.error('Error:', error);
 }
-
-
-    app.use(fileUpload(
-    {useTempFiles:true,
-     tempFileDir :'/temp'
-    }
-))
 
 app.use(cors({
   origin:'https://cloud-storage-zeta.vercel.app',
