abed8247a92299d20a39e51737fd568131ffc862
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-26 14:17:30 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const connectDB=async()=>{
     try {
      const connectionInst=await mongoose.connect(`${process.env.MONGO_URI}`)
-     console.log("MongoDB connected:",connectionInst.connection.host);
+     console.log("MongoDB connected:",connectionInst.connection.host,);
+        console.log('Current working directory:', process.cwd());
     } catch (error) {
         console.log('Unable to connect',error)
     }
