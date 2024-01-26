7e1148cc57f2267727150c9d8897dc5027968eb4
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-26 18:02:22 +0530

    Update upload.controller.js

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -1,4 +1,3 @@
-
 import { asyncHandler } from "../utility/asyncHandler.js";
 import { User } from "../models/users.model.js";
 import { uploadOnCloudinary } from "../utility/cloudinary.js";
@@ -68,57 +67,3 @@ const uploadFile = asyncHandler(async(req, res) => {
     });
   }
 });
-
-const uploadFile = asyncHandler(async(req, res) => {
-  try {
-      const form = new IncomingForm();
-
-    form.parse(req, async (err, fields, files) => {
-      if (err) {
-        console.error('Error parsing form:', err);
-        return res.status(500).json({ message: 'Internal server error' });
-      }
-    })
-      const file = files.files;
-    console.log (file)
-    if(file) {return res.status(400).json({message:"Select a file"})}
-    const path=req.files?.files.tempFilePath
-  /**********   Storage calculate      ****************************************************** */
- const user = await User.findOne({email:req.user.email})
-  const fileSize = req.files.files.size
-  const ocupied = user.stored
-  const total =ocupied+fileSize
-  if(total>process.env.MAX_STORAGE_LIMIT) return res.status(400).json({message:"Exceeding storage limit"})
- /****************Get extention **************/
-   const fileName = req.files.files.name;
-const parts = fileName.split('.');
-const fileExtension = parts.pop();
-const onlyName = fileName.split('.'+fileExtension)
-    let imageUrl =await uploadOnCloudinary(path);
-    const {url,bytes,resource_type,public_id,duration}=imageUrl
-
-     user.stored = total
-     await user.save()
-
-    //save the url to database
-    await File.create({
-      uploaded_by:req.user.email,
-      name: onlyName[0],  //文件名称
-      path: url,   //存储路径
-      type: resource_type,   //类型，如：图片、视频
-      size: bytes,   //大小，单位kb
-      duration:duration,
-      public_id:public_id  ,
-      format:fileExtension
-    })
-    return res.status(201).json({message:"Uploaded successfully",path}) 
-  } catch (error) {
-    console.log("Error in getting the file :", error);
-    return res.status(500).json({
-      err: error,
-      message: "internal server eror",
-    });
-  }
-});
-
-export {uploadFile}
