4037346baf814dab0183c064ffce4a740dcf6c26
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-26 21:10:41 +0530

    Update cloudinary.js

--- utility/cloudinary.js
+++ utility/cloudinary.js
@@ -1,35 +1,25 @@
 import { v2 as cloudinary } from "cloudinary";
 
+
 cloudinary.config({ 
-  cloud_name: 'cloud-space', 
-  api_key: process.env.CLOUDINARY_API, 
-  api_secret: process.env.CLOUDINARY_SECRET_KEY 
-});
+    cloud_name: 'cloud-space', 
+    api_key: process.env.CLOUDINARY_API, 
+    api_secret: process.env.CLOUDINARY_SECRET_KEY 
+  });
 
-const uploadOnCloudinary = async (fileBuffer, fileName) => {
+  const uploadOnCloudinary =async (localFilePath)=>{
   try {
-    if (!fileBuffer) return null;
-
-    const response = await cloudinary.uploader.upload_stream(
-      { 
-        folder: './uploads', // Optional: Adjust as needed
-        public_id: fileName, 
-        resource_type: "auto" 
-      },
-      (error, result) => {
-        if (error) {
-          console.error('Error uploading to Cloudinary:', error);
-        } else {
-          console.log('File uploaded successfully to Cloudinary:', result);
-        }
-      }
-    ).end(fileBuffer);
-
-    return response;
+    if(!localFilePath) return null
+   const response= await cloudinary.uploader.upload(localFilePath,{
+        resource_type:"auto"
+    })
+    console.log('file is uploaded')
+    return response
   } catch (error) {
-    console.error("Unable to upload to Cloudinary", error);
-    return null;
+    console.log("unable to upload",error)
   }
-}
 
-export { uploadOnCloudinary };
+
+  }
+
+ export {uploadOnCloudinary}
