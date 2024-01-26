9ef45b0831fa7750ad19d6da9c6b9b2fd78832d9
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-26 13:34:29 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ import { registerUser } from "../controller/user.controller.js";
 import { loginUser } from "../controller/user.controller.js";
 import { uploadFile } from "../controller/upload.controller.js";
 import { auth } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/auth.middleware.js";
 import { Data } from "../controller/data.controller.js";
 import { deleteFromCloudinary } from "../controller/delete.controller.js";
 const router =Router()
@@ -11,7 +12,7 @@ router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 
 //  Secure routes  
-router.route("/upload").post(auth,uploadFile)
+router.route("/upload").post(auth,upload.single('files'),uploadFile)
 router.route("/delete/:data").delete(auth,deleteFromCloudinary)
 router.route("/").post(auth,Data)
 
