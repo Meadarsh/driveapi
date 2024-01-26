581aa17481d540a7e55a745f94749892aeb3cb9b
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-26 17:33:34 +0530

    Update upload.controller.js

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -2,8 +2,19 @@ import { asyncHandler } from "../utility/asyncHandler.js";
 import { User } from "../models/users.model.js";
 import { uploadOnCloudinary } from "../utility/cloudinary.js";
 import { File } from "../models/file.model.js";
+import { IncomingForm } from 'formidable';
+
 const uploadFile = asyncHandler(async(req, res) => {
   try {
+      const form = new IncomingForm();
+
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error('Error parsing form:', err);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+
+      const file = files.files;
     console.log (req)
     if(!req.files) {return res.status(400).json({message:"Select a file"})}
     const path=req.files?.files.tempFilePath
