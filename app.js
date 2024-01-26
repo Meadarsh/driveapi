a9e7799e3093cc39a2258044cb7bb7ff7e040385
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-01-26 13:37:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,21 +4,7 @@ import cookieParser from "cookie-parser"
 import fileUpload from "express-fileupload";
 const app=express()
 
-try {
-  // Create the directory and its parent directories if they don't exist
-  fs.mkdirSync(tempDir, { recursive: true });
-  console.log(`Directory '${tempDir}' created successfully.`);
 
-  // Configure express-fileupload to use the correct temporary directory
-  app.use(
-    fileUpload({
-      useTempFiles: true,
-      tempFileDir: tempDir,
-    })
-  );} catch (error) {
-  // Handle any errors that occur during directory creation
-  console.error('Error:', error);
-}
 
 app.use(cors({
   origin:'https://cloud-storage-zeta.vercel.app',
